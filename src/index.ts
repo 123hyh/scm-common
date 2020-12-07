e38966c39a44ec3556e8c7b6acd1db869bc70e59
123hyh <310515005@qq.com>
2020-12-07 23:47:39 +0800

    删除

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: huangyuhui
- * @Date: 2020-12-03 16:54:29
- * @LastEditors: huangyuhui
- * @LastEditTime: 2020-12-03 16:58:46
- * @Description: 
- * @FilePath: \scm_frontend_common\src\index.ts
- */
