375cb2b235c1a32c226e05f1f6f3cf96c4bd32f6
huangyuhui <310515005@qq.com>
2020-12-08 14:56:56 +0800

    修改表格schema

--- typings/vue-component/tableSchema.d.ts
+++ typings/vue-component/tableSchema.d.ts
@@ -2,15 +2,15 @@
  * @Author: huangyuhui
  * @Date: 2020-11-30 16:21:17
  * @LastEditors: huangyuhui
- * @LastEditTime: 2020-12-01 19:01:34
+ * @LastEditTime: 2020-12-08 11:54:44
  * @Description: 表格 schema 类型
- * @FilePath: \customs\src\components\common\Table\tableSchema.d.ts
+ * @FilePath: \scm_frontend_common\typings\vue-component\tableSchema.d.ts
  */
 type generalType = {
   width?: string | number,
   align?: 'left' | 'center' | 'right',
   visible?: boolean,
-  label: string,
+  label?: string,
 
 }
 type ColumnType = generalType & {
