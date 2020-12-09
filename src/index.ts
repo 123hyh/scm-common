23bab95b6bb1c2b0b4d37cec2ef81a6d1dea8756
huangyuhui <310515005@qq.com>
2020-12-09 15:06:21 +0800

    修改组件导出

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@
  * @Author: huangyuhui
  * @Date: 2020-12-08 15:32:55
  * @LastEditors: huangyuhui
- * @LastEditTime: 2020-12-08 15:36:58
+ * @LastEditTime: 2020-12-09 15:05:22
  * @Description: 
  * @FilePath: \scm_frontend_common\src\index.ts
  */
-export * from './vue-component';
+export {
+  useScmComponent,
+  CombinationForm,
+  CombinationTable,
+  routerView,
+  ScmTabs
+} from './vue-component';
 export * from './utils';
 export * from './filters';
 export * from './directives';
