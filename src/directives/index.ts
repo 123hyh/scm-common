e051e2a17dd4a3abef655ea95cb24aa7871bded6
WIN-20200825LKR\Administrator <310515005@qq.com>
2021-01-03 23:50:38 +0800

    添加tooltip指令

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,9 +1,10 @@
 /*
  * @Author: huangyuhui
  * @Date: 2020-12-02 19:51:26
- * @LastEditors: huangyuhui
- * @LastEditTime: 2020-12-24 12:35:05
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-01-03 23:30:46
  * @Description: vue 指令
  * @FilePath: \scm_frontend_common\src\directives\index.ts
  */
 export { loading } from './loading';
+export { tooltip } from './tooltip';
