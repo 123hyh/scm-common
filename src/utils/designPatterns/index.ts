f2e47d9d6b0255a0f6d078194e2444764bf559e7
huangyuhui <310515005@qq.com>
2021-01-20 15:47:30 +0800

    修改

--- src/utils/designPatterns/index.ts
+++ src/utils/designPatterns/index.ts
@@ -2,9 +2,9 @@
  * @Author: huangyuhui
  * @Date: 2021-01-20 15:37:43
  * @LastEditors: huangyuhui
- * @LastEditTime: 2021-01-20 15:40:35
+ * @LastEditTime: 2021-01-20 15:46:55
  * @Description: 
  * @FilePath: \scm_frontend_common\src\utils\designPatterns\index.ts
  */
 
-export * from './LiabilityChain';
+export { when, useLiabilityChain } from './LiabilityChain';
