83de3b81924ea51310cde7e8a84f186675c02b6e
huangyuhui <310515005@qq.com>
2021-04-20 10:28:05 +0800

    update

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: huangyuhui
  * @Date: 2020-12-02 19:45:39
- * @LastEditors: huangyuhui
- * @LastEditTime: 2020-12-04 16:44:14
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-04-20 10:26:22
  * @Description: 过滤器
  * @FilePath: \scm_frontend_common\src\filters\index.ts
  */
@@ -10,6 +10,11 @@
 /* 比例转换 */
 export { numberToRatio } from './ratio';
 export { ratioToNumber } from './ratio';
+
+/**
+ * 数字 api
+ */
+export * from './number';
 
 /* date 过滤器 */
 export { formatDate } from './date';
