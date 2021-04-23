1d0a6317a819aff935b821591e112204a6968081
huangyuhui <310515005@qq.com>
2021-04-23 12:06:30 +0800

    查询栏 组件挂载时 隐藏移除

--- src/vue-component/QueryBar/index.js
+++ src/vue-component/QueryBar/index.js
@@ -2,7 +2,7 @@
  * @Author: huangyuhui
  * @Date: 2020-09-21 15:55:42
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-04-22 17:15:39
+ * @LastEditTime: 2021-04-23 12:05:43
  * @Description: 查询栏组件
  * @FilePath: \scm_frontend_common\src\vue-component\QueryBar\index.js
  */
@@ -372,6 +372,8 @@ export default {
 
     const unWatch = this.$watch( 'hideMore', this.debounceHandlerHide );
 
+    /* 首次加载先隐藏一次 */
+    this.handlerHide();
   
     this.$once( 'hook:beforeDestroy', unWatch );
   }
